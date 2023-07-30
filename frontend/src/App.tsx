d837cb23edbf68f88170f9f942c222509c50f822
Lee Swindell <39568393+LeeSwindell@users.noreply.github.com>
2023-07-30 11:02:31 -0400

    fonts, backend added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 function App() {
 
+  
+
   return (
     <>
-    <div className='flex flex-col w-full h-screen border justify-center place-items-center'>
+    <div className='flex flex-col w-full h-screen justify-center place-items-center'>
       <div className='text-center p-4' >ahhhhdsakdsajd</div>
-      <div className='text-center p-4 text-purple-400' >ahhhhdsfdsafdsafdsafakdsajd</div>
+      <div className='text-center p-4 text-violet-400' >ahhhhdsfdsafdsafdsafakdsajd</div>
       <div className='text-center p-4' >ahhhhdsakdsajd</div>
-      <div className='text-center p-4' >ahhhhdsakddsasjd</div>
+      <div className='text-center p-4 font-bold' >Bing Bong</div>
+      <button className='text-center p-4 rounded-lg border border-violet-400 hover:shadow hover:shadow-sky-50'>
+        Play
+      </button>
     </div>
     </>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,9 @@ export default {
   ],
   theme: {
     extend: {
-      // fontFamily:
+      fontFamily: {
+        sans: ['Roboto mono'],
+      }
     },
   },
   plugins: [],
